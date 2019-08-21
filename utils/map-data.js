512f22d2f36f6ebc77c5bd45e6d5af7bc9251ad7
Chris <cjmarsh725@gmail.com>
2019-08-20 18:31:57 -0700

    Map json file created

--- utils/map-data.js
+++ utils/map-data.js
@@ -1,11 +1,23 @@
 const sqlite = require("sqlite");
 const path = require("path");
+const fs = require("fs");
 
 const formatData = async () => {
   try {
     const db = await sqlite.open(path.resolve(__dirname, "data/eve-data.sqlite"));
-    const mapData = await db.get("SELECT * FROM mapSolarSystems WHERE solarSystemID = 30045354");
-    console.log(mapData);
+    const mapData = await db.all("SELECT * FROM mapSolarSystems");
+    const systems = mapData.map(sys => {
+      return {
+        name: sys.solarSystemName,
+        id: sys.solarSystemID,
+        regionID: sys.regionID,
+        security: sys.security,
+        x: sys.x,
+        y: sys.y,
+        z: sys.z,
+      };
+    });
+    fs.writeFile(path.resolve(__dirname, "data/map.json"), JSON.stringify(systems, null, 2), err => console.log(err));
   } catch (error) {
     console.log(error);
   }
